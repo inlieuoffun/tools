26a5f90001e81e914049878fe0f9c9371f382084
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-01-12 17:47:00 -0800

    epdate: put air date in the file

--- epdate/epdate.go
+++ epdate/epdate.go
@@ -205,7 +205,7 @@ func createEpisodeFile(path string, num int, desc string, up *ilof.TwitterUpdate
 		}
 		ep = &ilof.Episode{
 			Episode: ilof.Label(strconv.Itoa(num)),
-			Date:    ilof.Date(up.Date),
+			Date:    ilof.Date(up.AirDate),
 			Season:  int(float64(num)/250) + 1,
 			Detail:  desc,
 		}
