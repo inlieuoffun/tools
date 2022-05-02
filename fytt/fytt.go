91c25b636252fc4bd87275858f4cf68a78ec64da
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-05-02 06:53:50 -0700

    fytt: add -episode flag as an alternative to -id

--- fytt/fytt.go
+++ fytt/fytt.go
@@ -12,29 +12,44 @@ import (
 )
 
 var (
-	videoID = flag.String("id", "", "Video ID to fetch (required")
+	videoID = flag.String("id", "", "Video ID to fetch")
+	episode = flag.String("episode", "", "Episode number")
 )
 
 func main() {
 	flag.Parse()
-	switch {
-	case *videoID == "":
-		log.Fatal("You must set a non-empty video -id")
+	if *videoID == "" && *episode == "" {
+		log.Fatal("You must set a non-empty video -id or an -episode")
 	}
 
 	ctx := context.Background()
+	if *episode != "" {
+		ep, err := ilof.FetchEpisode(ctx, *episode)
+		if err != nil {
+			log.Fatalf("Fetching episode %q: %v", *episode, err)
+		}
+		id, ok := ilof.YouTubeVideoID(ep.YouTubeURL)
+		if !ok {
+			log.Fatalf("Unable to find video ID for episode %q", ep.Episode)
+		}
+		log.Printf("Found video ID %q for episode %q", id, ep.Episode)
+		*videoID = id
+	}
+
 	url, err := ilof.YouTubeCaptionURL(ctx, *videoID)
 	if err != nil {
 		log.Fatalf("Getting caption URL: %v", err)
 	} else if url == "" {
 		log.Fatalf("No caption URL found for video ID %q", *videoID)
 	}
+	log.Printf("Caption URL: %q", url)
 
 	cap, err := ilof.YouTubeCaptionData(ctx, url)
 	if err != nil {
 		log.Fatalf("Getting caption data: %v", err)
 	}
 	cap.VideoID = *videoID
+	log.Printf("Found %d captions for ID %q", len(cap.Captions), cap.VideoID)
 
 	// TODO(creachadair): Other output formats.
 	bits, err := json.Marshal(struct {
